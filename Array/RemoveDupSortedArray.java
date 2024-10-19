adb31d73f19734637005d4d903e9ade6ebec42cb
pranjalgoyal37 <148769596+pranjalgoyal37@users.noreply.github.com>
2024-10-19 11:23:18 +0530

    Update RemoveDupSortedArray.java

--- Array/RemoveDupSortedArray.java
+++ Array/RemoveDupSortedArray.java
@@ -1,3 +1,11 @@
 public  class RemoveDupSortedArray{
-
+ public int removeDuplicates(int[] nums) {
+           int left = 0;
+        // 0 0 3 3 5 6 
+        for(int i =1; i<nums.length; i++){
+            if(nums[left] != nums[i])
+                nums[++left] = nums[i];
+        }
+        return left+1;
+    }
 }
